ae8e8c46353f35539200db094897f4b0255461ba
Bulat Yusupov <yusupovbulat@gmail.com>
2021-01-17 13:34:02 +0500

    Added STATICFILES_DIRS constant

--- contactlist/settings.py
+++ contactlist/settings.py
@@ -118,5 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIR = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
