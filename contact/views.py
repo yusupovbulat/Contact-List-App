87c2e0ed7249ae5c7662ef6f8d2445533e45e808
Bulat Yusupov <yusupovbulat@gmail.com>
2021-01-19 00:23:32 +0500

    Create FBV to add contact

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Contact
 
 # Create your views here.
 
 
 def index(request):
     return render(request, "index.html")
+
+
+def add_contact(request):
+    if request.method == "POST":
+        new_contact = Contact(
+            full_name=request.POST['fullname'],
+            relationship=request.POST['relationship'],
+            email=request.POST['email'],
+            phone_number=request.POST['phone-number'],
+            address=request.POST['address']
+        )
+        new_contact.save()
+        return redirect("/")
+    return render(request, "new.html")
